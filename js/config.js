59806572f40ba5f253fa8276b69ddf158a2be428
Hao Liu <hao.liu@marklogic.com>
2019-08-05 00:02:46 +0000

    configure new user poolid and clientId for cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_LgjjZSBRp', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6lji6pia5pkj6ti1qvgt7srrdr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-2' // e.g. us-east-2
+        userPoolId: 'us-east-2_RiWrDJkin', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '41gio72t6lkr0c2kobqd72jeam', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
